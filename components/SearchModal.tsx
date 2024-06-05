ba2b6f0ab7e57d36b88a838365ffb78ae79228c4
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-05 12:16:16 +0300

    add search icons

---
+++ components/SearchModal.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+const SearchModal = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="" onClick={handleShow}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+        </svg>
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control type="search" placeholder="Search..." autoFocus />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+export default SearchModal;
--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import menuItems from "./menu.json";
 import Image from "next/image";
 import Link from "next/link";
+import SearchModal from "@/components/SearchModal";
 
 const Header = () => {
   return (
@@ -25,8 +26,18 @@ const Header = () => {
                 {item.title}
               </Nav.Link>
             ))}
+            <NavDropdown title="Pages" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/pages/contact" as={Link}>Contact</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/blog" as={Link}>Blog</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/authors" as={Link}>Authors</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/categories" as={Link}>Categories</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/tags" as={Link}>Tags</NavDropdown.Item>
+              <NavDropdown.Item href="/pages/error" as={Link}>404 Page</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
+
+        <SearchModal />
       </Container>
     </Navbar>
   );
