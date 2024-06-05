6a20b5f7c8fc02a22e42c9608d2cec60dd7505c8
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-05 19:25:51 +0300

    add homepage

--- components/homepage/index.tsx
+++ components/homepage/index.tsx
@@ -1,11 +1,19 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
+import "../../app/index.scss";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <h1>The Ultimate Starter Template You Need To Start Your Next Project</h1>
+      <p>
+        Nextplate is a free starter template built with Next and TailwindCSS,
+        providing everything you need to jumpstart your Next project and save
+        valuable time.
+      </p>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
