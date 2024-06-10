9bb4cec73793fcdd29a8b6589e6fdb801cc2431c
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-10 12:21:29 +0300

    add aboutpage

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import avatar from "../../../public/images/avatar.png";
+import Image from "next/image";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    
+    <div className="flex flex-col items-center justify-center gap-4 mt-20">
+      <Image src={avatar} className="mx-auto" alt="avatar" width={270} height={270} />
+      <h2 className="text-3xl font-bold">Hey, I am John Doe!</h2>
+      <p className="w-1/2 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum
+        nesciunt commodi vel nisi ut alias excepturi ipsum, totam, labore
+        tempora, odit ex iste tempore sed. Fugit voluptatibus perspiciatis
+        assumenda nulla ad nihil, omnis vel, doloremque sit quam autem optio
+        maiores, illum eius facilis et quo consectetur provident dolor
+        similique! Enim voluptatem dicta expedita veritatis repellat dolorum
+        impedit, provident quasi at.
+      </p>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
