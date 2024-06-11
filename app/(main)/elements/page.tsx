89a794d8a404aa3e34b38349cf3f0c33f04e71ee
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-11 12:45:26 +0300

    add elementspage

--- app/(main)/elements/page.tsx
+++ app/(main)/elements/page.tsx
@@ -2,6 +2,31 @@ import { BreadcrumbWithCustomSeparator } from "@/components/BreadcrumbWithCustom
 import React from "react";
 
 const ElementsPage = () => {
+  const htmlItemsString = `
+    <li class="nav-item">
+      <a class="nav-link" href="/">
+        Home
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="about/">
+        About
+      </a>
+    </li>`;
+
+  const cssItemsString = `
+    img {
+    vertical-align: middle;
+    border: 0;
+    max-width: 100%;
+    height: auto;
+    }`;
+
+  const jsItemsString = `
+    window.addEventListener("load", (e) => {
+      document.querySelector(".preloader").style.display = "none";
+    });`;
+
   return (
     <main>
       <section className="flex flex-col items-center bg-gray-100 p-24">
@@ -38,19 +63,106 @@ const ElementsPage = () => {
         </p>
         <hr className="text-gray-400" />
         <h3 className="text-3xl font-bold">Emphasis</h3>
-        <ol>
+        <ol className="flex flex-col gap-4 list-decimal pl-5">
           <li>
-            <span>1.</span>
-            <span>Did you come here for something in particular or just general</span>
+            Did you come here for something in <strong> particular</strong> or
+            just general
           </li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>
+            Did you come here for something in{" "}
+            <span className="underline">particular</span>
+          </li>
+          <li>
+            <span className="italic">Did you come here</span>
+          </li>
+          <li>
+            Did you come here for <strong>something</strong> in particular
+          </li>
+          <li>Did you come here for something in particular</li>
+          <li>Did you come here for something in particular</li>
+          <li>
+            URLs and URLs in angle brackets will automatically get turned into
+            links. <a href="http://www.example.com">http://www.example.com</a>{" "}
+          </li>
+          <li>
+            <a href="http://www.example.com" className="underline">
+              http://www.example.com
+            </a>{" "}
+            and sometimes example.com (but not on Github, for example).
+          </li>
         </ol>
+        <hr className="text-gray-400" />
+        <h3 className="text-3xl font-bold">Link</h3>
+        <p>
+          <a href="https://www.google.com" className="underline">
+            I'm an inline-style link
+          </a>
+        </p>
+        <p>
+          <a href="https://www.google.com" className="underline">
+            I'm an inline-style link with title
+          </a>
+        </p>
+        <p>
+          <a href="https://www.google.com" className="underline">
+            I'm a reference-style link
+          </a>
+        </p>
+        <p>
+          <a href="https://www.google.com" className="underline">
+            I'm a relative reference to a repository file
+          </a>
+        </p>
+        <p>
+          <a href="https://www.google.com" className="underline">
+            You can use numbers for reference-style link definitions
+          </a>
+        </p>
+        <p>
+          Or leave it empty and use the{" "}
+          <a href="https://www.google.com" className="underline">
+            link text itself
+          </a>
+        </p>
+        <p>example.com (but not on Github, for example).</p>
+        <p>Some text to show that the reference links can follow later.</p>
+        <hr className="text-gray-400" />
+        <h3 className="text-3xl font-bold">Ordered List</h3>
+        <ol className="flex flex-col gap-4 list-decimal pl-5">
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+        </ol>
+        <hr className="text-gray-400" />
+        <h3 className="text-3xl font-bold">Unordered List</h3>
+        <ul className="flex flex-col gap-4 list-disc pl-5">
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+          <li>List item</li>
+        </ul>
+        <h4 className="text-4xl font-bold">Code and Syntax Highlighting</h4>
+        <h3 className="text-3xl font-bold">HTML</h3>
+        <pre>
+          <code>
+            <ul className="bg-gray-100 p-1">{htmlItemsString}</ul>
+          </code>
+        </pre>
+        <h3 className="text-3xl font-bold">CSS</h3>
+        <pre>
+          <code>
+            <ul className="bg-gray-100 p-1">{cssItemsString}</ul>
+          </code>
+        </pre>
+        <h3 className="text-3xl font-bold">Javascript</h3>
+        <pre>
+          <code>
+            <ul className="bg-gray-100 p-1">{jsItemsString}</ul>
+          </code>
+        </pre>
       </section>
     </main>
   );
