d1fffca65f657a2f75d53fd2d3bbed831344b5fa
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-11 13:30:06 +0300

    add elementspage

--- app/(main)/elements/page.tsx
+++ app/(main)/elements/page.tsx
@@ -1,4 +1,5 @@
 import { BreadcrumbWithCustomSeparator } from "@/components/BreadcrumbWithCustomSeparator";
+import { ButtonOutline } from "@/components/ButtonOutline";
 import React from "react";
 
 const ElementsPage = () => {
@@ -163,6 +164,89 @@ const ElementsPage = () => {
             <ul className="bg-gray-100 p-1">{jsItemsString}</ul>
           </code>
         </pre>
+        <hr className="text-gray-400" />
+        <h3 className="text-3xl font-bold">Button</h3>
+        <div>
+          <ButtonOutline text="Button" />
+        </div>
+        <h3 className="text-3xl font-bold">Quote</h3>
+        <blockquote className="border-black rounded-xl border-t-2 border-r-2 border-b-2 border-l-8 p-5 bg-gray-100">
+          <p className="font-bold">
+            "Did you come here for something in particular or just general
+            Riker-bashing? And blowing into maximum warp speed, you appeared for
+            an instant to be in two places at once."
+          </p>
+        </blockquote>
+        <h3 className="text-3xl font-bold">Notice</h3>
+        <blockquote className="border-sky-400 rounded-xl border-2 p-5">
+          <div className="flex items-center gap-4 mb-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              fill="currentColor"
+              className="bi bi-info-circle text-sky-600"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+            </svg>
+            <span className="text-xl font-bold">Note</span>
+          </div>
+          <p>"This is a simple note."</p>
+        </blockquote>
+        <blockquote className="border-emerald-400 rounded-xl border-2 p-5">
+          <div className="flex items-center gap-4 mb-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              fill="currentColor"
+              className="bi bi-check-circle text-emerald-400"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05" />
+            </svg>
+            <span className="text-xl font-bold">Tip</span>
+          </div>
+          <p>"This is a simple note."</p>
+        </blockquote>
+        <blockquote className="border-orange-400 rounded-xl border-2 p-5">
+          <div className="flex items-center gap-4 mb-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              fill="currentColor"
+              className="bi bi-exclamation-triangle text-orange-400"
+              viewBox="0 0 16 16"
+            >
+              <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z" />
+              <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+            </svg>
+            <span className="text-xl font-bold">Info</span>
+          </div>
+          <p>"This is a simple note."</p>
+        </blockquote>
+        <blockquote className="border-red-400 rounded-xl border-2 p-5">
+          <div className="flex items-center gap-4 mb-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              fill="currentColor"
+              className="bi bi-x-circle text-red-400"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+            </svg>
+            <span className="text-xl font-bold">Warning</span>
+          </div>
+          <p>"This is a simple note."</p>
+        </blockquote>
+        <h3 className="text-3xl font-bold">Tab</h3>
       </section>
     </main>
   );
