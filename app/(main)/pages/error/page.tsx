a22b54b7a660d891e44c6f01900487e1b01ad68c
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-13 20:11:26 +0300

    add errorpage

--- app/(main)/pages/error/page.tsx
+++ app/(main)/pages/error/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <main>
+      <section>
+        <div className='container'>
+          <span className='text-7xl font-bold'>404</span>
+          <h1 className='text-3xl font-bold'>Page not found</h1>
+        </div>
+      </section>
+    </main>
   )
 }
 
