b04bb1b93c6779212820e47c489b098b666a62ac
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-13 19:41:43 +0300

    add categoriespage

--- app/(main)/pages/categories/page.tsx
+++ app/(main)/pages/categories/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { BreadcrumbWithCustomSeparator } from "@/components/BreadcrumbWithCustomSeparator";
+import React from "react";
 
 const CategoriesPage = () => {
   return (
-    <div>CategoriesPage</div>
-  )
-}
+    <main>
+      <section className="flex flex-col items-center bg-gray-100 p-24">
+        <h1 className="text-5xl pt-24 font-bold mb-3">Categories</h1>
+        <BreadcrumbWithCustomSeparator text="Categories" />
+      </section>
 
-export default CategoriesPage
+      <section>
+        <div>
+          <ul className="flex flex-wrap justify-center mt-32 gap-5">
+            <li>
+              <a href="" className="bg-gray-100 text-xl px-10 py-3 rounded-xl">
+                Application <span className="ml-3 px-3 bg-gray-50">1</span>{" "}
+              </a>
+            </li>
+            <li>
+              <a href="" className="bg-gray-100 text-xl px-10 py-3 rounded-xl">
+                Data <span className="ml-3 px-3 bg-gray-50">2</span>{" "}
+              </a>
+            </li>
+            <li>
+              <a href="" className="bg-gray-100 text-xl px-10 py-3 rounded-xl">
+                Technology <span className="ml-3 px-3 bg-gray-50">1</span>{" "}
+              </a>
+            </li>
+            <li>
+              <a href="" className="bg-gray-100 text-xl px-10 py-3 rounded-xl">
+                Software <span className="ml-3 px-3 bg-gray-50">1</span>{" "}
+              </a>
+            </li>
+            <li>
+              <a href="" className="bg-gray-100 text-xl px-10 py-3 rounded-xl">
+                Architecture <span className="ml-3 px-3 bg-gray-50">1</span>{" "}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default CategoriesPage;
