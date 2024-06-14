9c30fb694eea8d65846e22bb7b837acc0fc00b08
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-14 12:16:01 +0300

    errorpage finish

--- app/(main)/pages/error/page.tsx
+++ app/(main)/pages/error/page.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
 
 const ErrorPage = () => {
   return (
     <main>
-      <section>
-        <div className='container'>
-          <span className='text-7xl font-bold'>404</span>
-          <h1 className='text-3xl font-bold'>Page not found</h1>
+      <section className="p-32">
+        <div className="flex flex-col justify-center items-center gap-5 container">
+          <span className="text-9xl font-bold mt-24">404</span>
+          <h1 className="text-4xl font-bold">Page not found</h1>
+          <p>
+            The page you are looking for might have been removed, had its name
+            changed, or is temporarily unavailable.
+          </p>
+          <div>
+            <Button variant="outline">
+              <Link href="/">Back To Home</Link>
+            </Button>
+          </div>
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
