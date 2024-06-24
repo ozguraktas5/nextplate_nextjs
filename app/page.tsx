4cb9f77b8e7c20a814c50487a75ebefb4c12f126
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-24 18:45:59 +0300

    vercel deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import { Container } from "react-bootstrap";
-import React, { ReactNode } from "react";
+import React from "react";
 import HomePage from "@/components/homepage";
 
-interface HomeLayoutProps {
-  children: ReactNode;
-}
-
-const HomeLayout: React.FC<HomeLayoutProps> = () => {
+const HomeLayout: React.FC = () => {
   return (
     <>
       <Header />
